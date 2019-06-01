f9cbde74751396b1d46ce29c1e59051e60c72192
Nicholas Johnson <me@nicholasjohnson.com>
2019-06-01 18:19:33 +0100

    capitalisation

---
+++ src/components/Sidebar/Sidebar.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+import { Link } from "components/Router";
+import "./Sidebar.css";
+
+const Sidebar = () => (
+  <nav className="sidebar">
+    <h1>
+      <Link to="/">Emma Stone Johnson</Link>
+    </h1>
+    <ul>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+    </ul>
+  </nav>
+);
+
+export default Sidebar;
