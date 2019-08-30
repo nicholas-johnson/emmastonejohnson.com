28caed3c34f660068ec0d547d9813beaa7350bdb
Nicholas Johnson <me@nicholasjohnson.com>
2019-08-30 12:59:12 +0100

    fixes the about page

--- src/containers/About.tsx
+++ src/containers/About.tsx
@@ -1,27 +1,27 @@
 import React from "react";
-// import Navigation from "../components/Navigation";
-// import Header from "../components/Header";
-// import Footer from "../components/Footer";
-// import Typography from "../components/Typography";
-// import Content from "../components/Content";
+import Navigation from "../components/Navigation";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Typography from "../components/Typography";
+import Content from "../components/Content";
 
 import ArtistsStatement from "../content/ArtistsStatement";
 import AboutEmma from "../content/AboutEmma";
-// import EmmaStandingOnCanvas from "../content/Images/EmmaStandingOnCanvas";
+import EmmaStandingOnCanvas from "../content/Images/EmmaStandingOnCanvas";
 
 import "./About.scss";
 
 export default () => (
   <div className="about-page">
-    {/* <Typography> */}
-    {/* <Navigation /> */}
-    {/* <Header /> */}
-    {/* <Content> */}
-    {/* <EmmaStandingOnCanvas /> */}
-    <AboutEmma />
-    <ArtistsStatement />
-    {/* <Footer /> */}
-    {/* </Content> */}
-    {/* </Typography> */}
+    <Typography>
+      <Navigation />
+      <Header />
+      <Content>
+        <EmmaStandingOnCanvas />
+        <AboutEmma />
+        <ArtistsStatement />
+        <Footer />
+      </Content>
+    </Typography>
   </div>
 );
