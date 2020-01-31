7473893c9f65b04dda107296b4502130902e92e2
Nicholas Johnson <me@nicholasjohnson.com>
2020-01-31 12:29:49 +0000

    updated text

--- src/content/AboutEmma/AboutEmma.tsx
+++ src/content/AboutEmma/AboutEmma.tsx
@@ -8,5 +8,18 @@ export const AboutEmma = () => (
       with her prints and has worked in graphic design for the last 10 years,
       working on rebrands and concept development.
     </p>
+    <p>
+      Emma was one of three winners for the Habitat Brighton art competition
+      2019. Her painting "Dust" will be available to buy on a cusion in the
+      Brighton Habitat store and online, Autumn 2020.
+    </p>
+    <p>
+      Her paintings "Untitled Blue" and "Smashed Plate" are also featured in
+      George Clarke's Old House, New Home on Channel 4, Sunday 2nd February.
+    </p>
+    <p>
+      Emma is elated to be commencing a Painting MA at the Royal College of Art,
+      2020.
+    </p>
   </div>
 );
