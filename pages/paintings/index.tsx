77a1fa1892778fcc7b4d10e0ee21126d6a3e4e6c
Nicholas Johnson <me@nicholasjohnson.com>
2022-04-29 16:34:51 +0100

    feat(images): Increase image resolution

--- pages/paintings/index.tsx
+++ pages/paintings/index.tsx
@@ -1,13 +1,13 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import Image from 'next/image'
-import styles from './Paintings.module.scss'
-import content from '../../public/content.json'
-import Header from '../../components/Header'
-import ContentfulContent from '../../components/ContentfulContent'
-import { usePaintings } from '../../hooks/usePaintings'
-import { Document } from '@contentful/rich-text-types';
+import type { NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import styles from "./Paintings.module.scss";
+import content from "../../public/content.json";
+import Header from "../../components/Header";
+import ContentfulContent from "../../components/ContentfulContent";
+import { usePaintings } from "../../hooks/usePaintings";
+import { Document } from "@contentful/rich-text-types";
 
 const Home: NextPage = () => {
   const paintings = usePaintings();
@@ -22,22 +22,25 @@ const Home: NextPage = () => {
       </Head>
 
       <div className={styles.container}>
-
         <Header />
 
-        {
-        paintings
-          .filter(painting => painting.fields)
-          .map(painting => {
-            const height = painting.fields?.image?.fields?.file?.details?.image?.height || 1
-            const width = painting.fields?.image?.fields?.file?.details?.image?.width || 1;
+        {paintings
+          .filter((painting) => painting.fields)
+          .map((painting) => {
+            const height =
+              painting.fields?.image?.fields?.file?.details?.image?.height || 1;
+            const width =
+              painting.fields?.image?.fields?.file?.details?.image?.width || 1;
             return (
-              <article key={painting.fields?.permalink} className={styles.painting}>
-                <img 
-                  src={`${painting.fields?.image?.fields.file.url}?w=600`}
-                  alt={painting.fields?.name} 
+              <article
+                key={painting.fields?.permalink}
+                className={styles.painting}
+              >
+                <img
+                  src={`${painting.fields?.image?.fields.file.url}?w=1200`}
+                  alt={painting.fields?.name}
                   width="100%"
-                  height={`${width / height * 100}%`}
+                  height={`${(width / height) * 100}%`}
                 />
                 {/* <Image
                   src={`/paintings/${painting.fields?.permalink}.jpg`}
@@ -46,14 +49,15 @@ const Home: NextPage = () => {
                   height={painting.fields?.image?.fields.file.details.image.height}
                   layout="responsive"></Image> */}
                 <h2>{painting.fields?.name}</h2>
-                <ContentfulContent document={painting.fields?.description as Document} />
+                <ContentfulContent
+                  document={painting.fields?.description as Document}
+                />
               </article>
-            )
+            );
           })}
-
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
