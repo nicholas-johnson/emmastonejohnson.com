e4de58560a9433a272a37865400783bbaf2a1dfe
Nicholas Johnson <me@nicholasjohnson.com>
2022-05-01 12:09:00 +0100

    feat(content): cv and articles

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,12 @@ const Home: NextPage = () => {
           <li>
             <Link href="/about">About</Link>
           </li>
+          <li>
+            <Link href="/cv">CV</Link>
+          </li>
+          <li>
+            <Link href="/interviews">Interviews</Link>
+          </li>
         </ul>
       </div>
     </>
